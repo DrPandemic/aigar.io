da4289b8b53ab108875cf0b964746910434e152b
Jean-Samuel Aubry-Guzzi <bipbip500@gmail.com>
2016-09-23 17:01:04 -0400

    Trying to import libraries

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -4,39 +4,44 @@ const babel = require('gulp-babel');
 var sass = require('gulp-sass');
 var inject = require('gulp-inject');
 var clean = require('gulp-clean');
+var gnf = require('gulp-npm-files');
 
 var destination = 'dist/web/';
 
 gulp.task('sass', function () {
-    return gulp.src('src/scss/**/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass().on('error', sass.logError))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(destination + 'css'));
+  return gulp.src('src/scss/**/*.scss')
+             .pipe(sourcemaps.init())
+             .pipe(sass().on('error', sass.logError))
+             .pipe(sourcemaps.write())
+             .pipe(gulp.dest(destination + 'css'));
 });
 
 gulp.task('javascript', function() {
-    return gulp.src('src/javascript/**/*.js')
-        .pipe(sourcemaps.init())
-        .pipe(babel({
-            plugins: ['transform-runtime'],
-            presets: ['es2015']
-        }))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest(destination + 'javascript'));
+  return gulp.src('src/javascript/**/*.js')
+             .pipe(sourcemaps.init())
+             .pipe(babel({
+               plugins: ['transform-runtime'],
+               presets: ['es2015']
+             }))
+             .pipe(sourcemaps.write('.'))
+             .pipe(gulp.dest(destination + 'javascript'));
 });
 
-gulp.task('html', ['javascript', 'sass'], function() {
-    var target = gulp.src('src/index.html');
-    var sources = gulp.src(['./dist/javascript/**/*.js', './dist/css/**/*.css'], {read: false});
+gulp.task('vendor', function() {
+  gulp.src(gnf(), {base:'./node_modules/'}).pipe(gulp.dest(destination + 'vendor'));
+});
 
-    return target.pipe(inject(sources))
-        .pipe(gulp.dest(destination));
+gulp.task('html', ['javascript', 'vendor', 'sass'], function() {
+  var target = gulp.src('src/index.html');
+  var sources = gulp.src([destination + 'javascript/**/*.js', destination + 'css/**/*.css'], {read: false});
+
+  return target.pipe(inject(sources))
+               .pipe(gulp.dest(destination));
 });
 
 gulp.task('clean', function () {
-    return gulp.src('dist/*', {read: false})
-        .pipe(clean());
+  return gulp.src('dist/*', {read: false})
+             .pipe(clean());
 });
 
 gulp.task('default', ['html']);
