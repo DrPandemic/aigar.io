8a6d063ae59c78a591e122b0f4341924a444f0f6
Jean-Samuel Aubry-Guzzi <bipbip500@gmail.com>
2016-09-26 16:39:46 -0400

    Adds default leadboard route

--- game/src/main/scala/ScalatraBootstrap.scala
+++ game/src/main/scala/ScalatraBootstrap.scala
@@ -1,9 +1,10 @@
-import io.aigar._
+import io.aigar.servlet._
 import org.scalatra._
 import javax.servlet.ServletContext
 
 class ScalatraBootstrap extends LifeCycle {
-  override def init(context: ServletContext) {
-    context.mount(new AigarServlet, "/*")
+  override def init(context: ServletContext): Unit = {
+    val path = "/api/1"
+    context.mount(new LeaderboardController(), s"$path/leaderboard/*")
   }
 }
---
+++ game/src/main/scala/io/aigar/servlet/LeaderboardController.scala
@@ -0,0 +1,22 @@
+package io.aigar.servlet
+
+import org.json4s.{DefaultFormats, Formats}
+import org.scalatra.json._
+
+class LeaderboardController extends AigarStack with JacksonJsonSupport {
+  protected implicit val jsonFormats: Formats = DefaultFormats
+
+  before() {
+    contentType = formats("json")
+  }
+
+  case class Leaderboard(name: String)
+
+  get("/") {
+    List(
+      Leaderboard("asd"),
+      Leaderboard("another")
+    )
+
+  }
+}
