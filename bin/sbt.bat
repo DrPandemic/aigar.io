4ab5c22b05748ef5855fef6494f3e7f6c892b00b
Mic Lamb <michael.o.lambert@gmail.com>
2016-09-27 14:50:53 -0400

    Fixes start.bat and stop.bat
    Corrects the start.bat file and creates the correct stop.bat file.

--- bin/sbt.bat
+++ bin/sbt.bat
@@ -1,4 +1,3 @@
 cd ../game
 sbt "~;jetty:stop;jetty:start"
 cd ../bin
-echo sbt started
--- bin/start.bat
+++ bin/start.bat
@@ -10,15 +10,13 @@ if not "%~1"=="-p" goto UNKNOWN
 
 :SPECIFIC
 
-set PREFIX_PATH = %2
+set PREFIX_PATH=%2
 start build.bat
+start sbt.bat
 
 cd ../api/nginx-1.11.4-windows
 nginx -c "%PREFIX_PATH%/conf" -p "%PREFIX_PATH%"
-echo nginx started
 cd ../../bin
-
-start sbt.bat
 
 goto DONE
 
@@ -35,10 +33,3 @@ echo Unknown Option %1
 goto DONE
 
 :DONE
-
-echo Done!
-
-
-
-
-
