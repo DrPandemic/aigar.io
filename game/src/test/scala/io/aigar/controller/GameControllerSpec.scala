20d41ef321453e12b4b8e1c82ca18bef2ad922b8
Mic Lamb <michael.o.lambert@gmail.com>
2016-10-02 16:27:19 -0400

    Adds Helper for tests
    Allows the controllers to use an in-memory database instead of the one we are using in production.

--- game/src/test/scala/io/aigar/controller/GameControllerSpec.scala
+++ game/src/test/scala/io/aigar/controller/GameControllerSpec.scala
@@ -1,13 +1,10 @@
 import io.aigar.controller._
 
 import org.scalatra.test.specs2._
-import slick.driver.H2Driver.api._
-import com.mchange.v2.c3p0.ComboPooledDataSource
+import io.aigar.HelperSpec
 
-class GameControlleSpec extends MutableScalatraSpec {
-  val cpds = new ComboPooledDataSource
-  val db = Database.forDataSource(cpds) //Should be changed (in memory)
-  addServlet(new GameController(db), "/*")
+class GameControllerSpec extends MutableScalatraSpec with HelperSpec {
+  addServlet(new GameController(getDatabase()), "/*")
 
   "GET / on GameController" should {
     "return status 200" in {
--- game/src/test/scala/io/aigar/controller/LeaderboardControllerSpec.scala
+++ game/src/test/scala/io/aigar/controller/LeaderboardControllerSpec.scala
@@ -1,13 +1,10 @@
 import io.aigar.controller._
 
 import org.scalatra.test.specs2._
-import slick.driver.H2Driver.api._
-import com.mchange.v2.c3p0.ComboPooledDataSource
+import io.aigar.HelperSpec
 
-class LeaderboardControllerSpec extends MutableScalatraSpec {
-  val cpds = new ComboPooledDataSource
-  val db = Database.forDataSource(cpds) //Should be changed (in memory)
-  addServlet(new LeaderboardController(db), "/*")
+class LeaderboardControllerSpec extends MutableScalatraSpec with HelperSpec{
+  addServlet(new LeaderboardController(getDatabase()), "/*")
 
   "GET / on LeaderboardController" should {
     "return status 200" in {
