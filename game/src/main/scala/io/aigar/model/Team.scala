fb96a488d5dac18e5e23ca3d0332217676e82846
Mic Lamb <michael.o.lambert@gmail.com>
2016-10-05 10:10:44 -0400

    Adds the table's class
    Adds the table's class and defines the attributes in the database.

--- game/src/main/scala/io/aigar/model/Team.scala
+++ game/src/main/scala/io/aigar/model/Team.scala
@@ -1,5 +1,15 @@
 package io.aigar.model
 
-case class Team()
+import slick.driver.H2Driver.api._
+
+case class Team(id:Option[Int], teamSecret:String, teamName:String, score:Int)
+
+class Teams(tag: Tag) extends Table[(Int, String, String, Int)](tag, "TEAMS") {
+  def id = column[Int]("ID", O.PrimaryKey)
+  def teamSecret = column[String]("TEAM_SECRET")
+  def teamName = column[String]("TEAM_NAME")
+  def score = column[Int]("SCORE", O.Default(0))
+  def * = (id, teamSecret, teamName, score)
+}
 
 
