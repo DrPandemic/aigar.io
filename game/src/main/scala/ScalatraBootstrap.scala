9ab369fd2cc14739a69cb8a0459c52ebe0d8797c
JesseEmond <emond.jesse@gmail.com>
2016-10-04 23:44:48 -0400

    Create GameThread on init

--- game/src/main/scala/ScalatraBootstrap.scala
+++ game/src/main/scala/ScalatraBootstrap.scala
@@ -4,8 +4,14 @@ import javax.servlet.ServletContext
 
 class ScalatraBootstrap extends LifeCycle {
   override def init(context: ServletContext): Unit = {
+    launchGameLoop
+
     val path = "/api/1"
     context.mount(new LeaderboardController(), s"$path/leaderboard/*")
     context.mount(new GameController(), s"$path/game/*")
+  }
+
+  def launchGameLoop {
+    new Thread(new GameThread).start
   }
 }
---
+++ game/src/main/scala/io/aigar/game/GameThread.scala
@@ -0,0 +1,10 @@
+/**
+ * GameThread is the thread that runs continuously through the competition that
+ * takes care of updating the individual games and processing the queued inputs
+ * of the players.
+ */
+class GameThread extends Runnable {
+  def run {
+    println("TODO Game loop")
+  }
+}
