1c29dde2b7adfeef6f42a2fcaf1bd0c10c59a79f
JesseEmond <emond.jesse@gmail.com>
2016-10-06 01:03:37 -0400

    Refactor build.scala and configure IntegrationTest

--- game/project/build.scala
+++ game/project/build.scala
@@ -13,44 +13,51 @@ object AigarBuild extends Build {
   val ScalaVersion = "2.11.8"
   val ScalatraVersion = "2.4.1"
 
-  lazy val project = Project (
-    "aigar",
-    file("."),
-    settings = ScalatraPlugin.scalatraSettings ++ scalateSettings ++ Seq(
+  lazy val project = Project ("aigar", file("."))
+    .configs(IntegrationTest)
+    .settings(ScalatraPlugin.scalatraSettings)
+    .settings(scalateSettings)
+    .settings(Defaults.itSettings : _*)
+    .settings(projectSettings)
+    .enablePlugins(JettyPlugin)
+
+  lazy val projectSettings = Seq(
       organization := Organization,
       name := Name,
       version := Version,
       scalaVersion := ScalaVersion,
       resolvers += Classpaths.typesafeReleases,
       resolvers += "Scalaz Bintray Repo" at "http://dl.bintray.com/scalaz/releases",
-      libraryDependencies ++= Seq(
-        "org.scalatra" %% "scalatra" % ScalatraVersion,
-        "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
-        "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test",
-        "org.slf4j" % "slf4j-api" % "1.7.13" % "provided",
-        "org.slf4j" % "slf4j-nop" % "1.7.13" % "test",
-        "org.eclipse.jetty" % "jetty-webapp" % "9.2.15.v20160210" % "container",
-        "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided",
-        "org.json4s" %% "json4s-jackson" % "3.3.0.RC2",
-        "org.scalatra" %% "scalatra-json" % "2.4.0-RC2-2",
-        "com.typesafe.slick" %% "slick" % "3.1.1",
-        "com.h2database" % "h2" % "1.4.192",
-        "com.mchange" % "c3p0" % "0.9.5.1",
-        "org.scalactic" %% "scalactic" % "3.0.0",
-        "org.scalatest" %% "scalatest" % "3.0.0" % "test"
-      ),
-      scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
-        Seq(
-          TemplateConfig(
-            base / "webapp" / "WEB-INF" / "templates",
-            Seq.empty,  /* default imports should be added here */
-            Seq(
-              Binding("context", "_root_.org.scalatra.scalate.ScalatraRenderContext", importMembers = true, isImplicit = true)
-            ),  /* add extra bindings here */
-            Some("templates")
-          )
+      libraryDependencies ++= deps,
+      scalateTemplates)
+
+  lazy val deps = Seq(
+    "org.scalatra" %% "scalatra" % ScalatraVersion,
+    "org.scalatra" %% "scalatra-scalate" % ScalatraVersion,
+    "org.scalatra" %% "scalatra-specs2" % ScalatraVersion % "test,it",
+    "org.slf4j" % "slf4j-api" % "1.7.13" % "provided",
+    "org.slf4j" % "slf4j-nop" % "1.7.13" % "test,it",
+    "org.eclipse.jetty" % "jetty-webapp" % "9.2.15.v20160210" % "container",
+    "javax.servlet" % "javax.servlet-api" % "3.1.0" % "provided",
+    "org.json4s" %% "json4s-jackson" % "3.3.0.RC2",
+    "org.scalatra" %% "scalatra-json" % "2.4.0-RC2-2",
+    "com.typesafe.slick" %% "slick" % "3.1.1",
+    "com.h2database" % "h2" % "1.4.192",
+    "com.mchange" % "c3p0" % "0.9.5.1",
+    "org.scalactic" %% "scalactic" % "3.0.0",
+    "org.scalatest" %% "scalatest" % "3.0.0" % "test,it"
+      )
+  lazy val scalateTemplates =
+    scalateTemplateConfig in Compile <<= (sourceDirectory in Compile){ base =>
+      Seq(
+        TemplateConfig(
+          base / "webapp" / "WEB-INF" / "templates",
+          Seq.empty,  /* default imports should be added here */
+         Seq(
+           Binding("context", "_root_.org.scalatra.scalate.ScalatraRenderContext", importMembers = true, isImplicit = true)
+         ),  /* add extra bindings here */
+        Some("templates")
         )
-      }
-    )
-  ).enablePlugins(JettyPlugin)
+      )
+    }
 }
