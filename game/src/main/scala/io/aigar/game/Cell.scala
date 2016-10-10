fd405d75a6378338fb15a4202c07e112dac01253
JesseEmond <emond.jesse@gmail.com>
2016-10-10 16:17:03 -0400

    Implement cell movement based on mass

--- game/src/main/scala/io/aigar/game/Cell.scala
+++ game/src/main/scala/io/aigar/game/Cell.scala
@@ -2,11 +2,46 @@ package io.aigar.game
 
 import com.github.jpbetz.subspace._
 
+object Cell {
+  /**
+   * Force (in Newtons) applied when moving.
+   */
+  final val MovementForce = 10f
+
+  /**
+   * Default mass of a cell.
+   */
+  final val DefaultMass = 1
+}
+
 class Cell {
   var position = new Vector2(0f, 0f)
   var target = new Vector2(0f, 0f)
+  var mass = Cell.DefaultMass
+  private var _velocity = new Vector2(0f, 0f)
 
-  def update {
+  /**
+   * The maximum speed (length of the velocity) for the cell, in units per
+   * second.
+   */
+  def maxSpeed = {
+    15f //TODO depend on mass?
+  }
 
+  def velocity = _velocity
+  def velocity_=(vel:Vector2) {
+    _velocity = if (vel.magnitude < maxSpeed) vel else vel.normalize * maxSpeed
+  }
+
+
+  def update(deltaSeconds: Float) {
+    velocity += acceleration * deltaSeconds
+    position += velocity * deltaSeconds
+  }
+
+  def acceleration: Vector2 = {
+    val dir = target - position
+    val value = Cell.MovementForce / mass
+    return if (dir.magnitude > 0) dir.normalize * value else new Vector2(0f,0f)
   }
 }
--- game/src/test/scala/io/aigar/game/CellSpec.scala
+++ game/src/test/scala/io/aigar/game/CellSpec.scala
@@ -4,12 +4,48 @@ import com.github.jpbetz.subspace._
 
 class CellSpec extends FlatSpec with Matchers {
   "A Cell" should "not move when its target is on itself" in {
-    val cell = new Cell()
+    val cell = new Cell
     cell.position = new Vector2(42f, 42f)
     cell.target = new Vector2(42f, 42f)
-    cell.update
 
-    cell.position should equal(new Vector2(42, 42))
+    cell.update(1f)
+
+    cell.position should equal(new Vector2(42f, 42f))
+  }
+  
+  it should "move towards its target when it is away from itself" in {
+    val cell = new Cell
+    cell.position = new Vector2(42f, 42f)
+    cell.target = new Vector2(1000f, 1000f)
+
+    val initialDistance = cell.position.distanceTo(cell.target)
+
+    cell.update(1f)
+
+    val finalDistance = cell.position.distanceTo(cell.target)
+    
+    initialDistance should be > finalDistance
+  }
+
+  it should "have a maximal velocity" in {
+    val cell = new Cell
+
+    val velocity = new Vector2(1000f, 1000f)
+    cell.velocity = velocity
+
+    cell.velocity should not equal(velocity)
+  }
+
+  it should "move faster when it has a small mass" in {
+    val small = new Cell
+    small.target = new Vector2(100f, 100f)
+    val big = new Cell
+    big.target = new Vector2(100f, 100f)
+
+    small.mass = 1
+    big.mass = 1000
+
+    small.acceleration.magnitude should be > big.acceleration.magnitude
   }
 }
 
