ed0483a245d586e814d8bea53d793fd678c895d2
JesseEmond <emond.jesse@gmail.com>
2016-10-10 22:15:56 -0400

    Add Player class

---
+++ game/src/test/scala/io/aigar/game/PlayerSpec.scala
@@ -0,0 +1,26 @@
+import io.aigar.game._
+import org.scalatest._
+import org.scalatest.LoneElement._
+import com.github.jpbetz.subspace._
+
+class PlayerSpec extends FlatSpec with Matchers {
+  "A Player" should "start with a cell at its start position" in {
+    val player = new Player(new Vector2(42f, 42f))
+    
+    player.cells.loneElement.position should equal(new Vector2(42f, 42f))
+   }
+
+   it should "move its cells on update" in {
+     val player = new Player(new Vector2(0f, 0f))
+     val target = new Vector2(100f, 100f)
+     player.cells.head.target = target
+
+     val initialDistance = player.cells.head.position.distanceTo(target)
+
+     player.update(1f)
+
+     val finalDistance = player.cells.head.position.distanceTo(target)
+     
+     initialDistance should be > finalDistance
+   }
+}
