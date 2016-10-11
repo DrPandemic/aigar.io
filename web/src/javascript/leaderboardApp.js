4c3eaa8d7d1025a167ad0c60fddee1d3ec693d58
DrPandemic <bipbip500@gmail.com>
2016-10-10 21:28:22 -0400

    Encodes HTML entities

--- web/src/javascript/leaderboardApp.js
+++ web/src/javascript/leaderboardApp.js
@@ -1,12 +1,13 @@
 import {fetchEntries} from "./leaderboardEntry";
+import he from "he";
 
 function displayEntries(entries) {
   const leaderboard = document.getElementById("leaderboard-body");
   for(const entry of entries) {
     const row = leaderboard.insertRow(0);
-    row.insertCell(0).innerHTML = entry.team_id;
-    row.insertCell(1).innerHTML = entry.name;
-    row.insertCell(2).innerHTML = entry.score;
+    row.insertCell(0).innerHTML = he.encode(entry.team_id.toString());
+    row.insertCell(1).innerHTML = he.encode(entry.name);
+    row.insertCell(2).innerHTML = he.encode(entry.score.toString());
   }
 }
 
