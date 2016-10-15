7cab9b0ae7979e73a2590991964b0693cc9fd54a
Mic Lamb <michael.o.lambert@gmail.com>
2016-10-15 14:41:37 -0400

    Removes Sequential keyword
    Forgot to remove it -_-

--- game/src/it/scala/ScalatraBootstrapSpec.scala
+++ game/src/it/scala/ScalatraBootstrapSpec.scala
@@ -11,7 +11,6 @@ import org.specs2.matcher._
 class ScalatraBootstrapSpec extends WholeAppTest {
   protected implicit val jsonFormats: Formats = DefaultFormats
 
-  sequential
   "Launching the whole application" should {
     "launch a game thread that sets the state of the ranked game" in {
       get("/api/1/game/" + Game.RankedGameId.toString) {
