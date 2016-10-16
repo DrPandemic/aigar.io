da652343b23f4f60f8becbb8a255fa049b431eb2
DrPandemic <bipbip500@gmail.com>
2016-10-16 13:14:19 -0400

    Adds color to leaderboard

--- web/src/javascript/gameLeaderboard.js
+++ web/src/javascript/gameLeaderboard.js
@@ -1,4 +1,5 @@
 import he from "he";
+import {getPlayerColor} from "./drawMap";
 
 export function drawLeaderboard(state) {
   const leaderboard = document.getElementById("leaderboard-body");
@@ -7,10 +8,12 @@ export function drawLeaderboard(state) {
   const players = state.players.sort((a, b) => a.score - b.score);
 
   for(const player of players) {
+    const color = getPlayerColor(state.players, player);
     const row = new_tbody.insertRow(0);
-    row.insertCell(0).innerHTML = he.encode(player.id.toString());
-    row.insertCell(1).innerHTML = he.encode(player.name);
-    row.insertCell(2).innerHTML = he.encode(player.total_mass.toString());
+    row.insertCell(0).innerHTML = `<div class="color-box" style="background-color: ${color};"></div>`;
+    row.insertCell(1).innerHTML = he.encode(player.id.toString());
+    row.insertCell(2).innerHTML = he.encode(player.name);
+    row.insertCell(3).innerHTML = he.encode(player.total_mass.toString());
   }
 
   leaderboard.parentNode.replaceChild(new_tbody, leaderboard);
