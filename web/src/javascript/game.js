042fa246335b2ca3f034932459d5d1bb5e35520d
DrPandemic <bipbip500@gmail.com>
2016-10-17 15:12:34 -0400

    Removes unused fucntions

--- web/src/javascript/game.js
+++ web/src/javascript/game.js
@@ -8,23 +8,12 @@ import {
 
 const apiURL = "http://localhost:1337/api/1/game/";
 
-function extractPointsFromState(gameState) {
-  return [].concat.apply(...gameState.players.map((p) => p.cells));
-}
-
 export function draw(gameState, canvas) {
   initMap(canvas);
   drawFoodOnMap(gameState.food, canvas);
   drawPlayersOnMap(gameState.players, canvas);
   drawMap(canvas);
   drawMiniMap(canvas);
-}
-
-export function update(gameState) {
-  // To do a deep copy. It's seems to be the best solution
-  const newState = JSON.parse(JSON.stringify(gameState));
-
-  return newState;
 }
 
 export function fetchState(gameId) {
