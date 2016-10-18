1c96b736912457a29dc893f91b0ea354d3810419
Mic Lamb <michael.o.lambert@gmail.com>
2016-10-18 17:31:18 -0400

    Adds parameters for max value of each resources
    Adds parameters for max value of each resources

--- game/src/main/scala/io/aigar/game/Resources.scala
+++ game/src/main/scala/io/aigar/game/Resources.scala
@@ -2,21 +2,27 @@ package io.aigar.game
 
 import io.aigar.game.serializable.Position
 
+object Resources {
+  final val MaxRegular = 200
+  final val MaxSilver = 100
+  final val MaxGold = 50
+}
+
 class Resources {
   var regular = initRegular
   var silver = initSilver
   var gold = initGold
 
   def initRegular: List[Position] ={
-    List.fill(200)(Position(scala.util.Random.nextInt(900*3), scala.util.Random.nextInt(700*3)))
+    List.fill(Resources.MaxRegular)(Position(scala.util.Random.nextInt(900*3), scala.util.Random.nextInt(700*3)))
   }
 
   def initSilver: List[Position] ={
-    List.fill(100)(Position(scala.util.Random.nextInt(900*3), scala.util.Random.nextInt(700*3)))
+    List.fill(Resources.MaxSilver)(Position(scala.util.Random.nextInt(900*3), scala.util.Random.nextInt(700*3)))
   }
 
   def initGold: List[Position] ={
-    List.fill(50)(Position(scala.util.Random.nextInt(900*3), scala.util.Random.nextInt(700*3)))
+    List.fill(Resources.MaxGold)(Position(scala.util.Random.nextInt(900*3), scala.util.Random.nextInt(700*3)))
   }
 
   def state = {
