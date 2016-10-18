f8fb2e9709311168ca1e9e88f00bd9c89c528a45
Mic Lamb <michael.o.lambert@gmail.com>
2016-10-18 17:04:31 -0400

    Adds resources on the map randomly.
    Adds resources on the map randomly.

--- game/src/main/scala/io/aigar/game/Resources.scala
+++ game/src/main/scala/io/aigar/game/Resources.scala
@@ -3,9 +3,21 @@ package io.aigar.game
 import io.aigar.game.serializable.Position
 
 class Resources {
-  var regular = List(new Position(12, 12))
-  var silver = List(new Position(112, 112))
-  var gold = List(new Position(212, 212))
+  var regular = initRegular
+  var silver = initSilver
+  var gold = initGold
+
+  def initRegular: List[Position] ={
+    List.fill(200)(Position(scala.util.Random.nextInt(900*3), scala.util.Random.nextInt(700*3)))
+  }
+
+  def initSilver: List[Position] ={
+    List.fill(100)(Position(scala.util.Random.nextInt(900*3), scala.util.Random.nextInt(700*3)))
+  }
+
+  def initGold: List[Position] ={
+    List.fill(50)(Position(scala.util.Random.nextInt(900*3), scala.util.Random.nextInt(700*3)))
+  }
 
   def state = {
     serializable.Resources(
