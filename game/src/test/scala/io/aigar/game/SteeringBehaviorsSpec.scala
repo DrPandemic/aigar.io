72b1b0223d5327647f91a96802ae1c2a4641312a
JesseEmond <emond.jesse@gmail.com>
2016-10-22 02:53:20 -0400

    Update tests

--- game/src/test/scala/io/aigar/game/SteeringBehaviorsSpec.scala
+++ game/src/test/scala/io/aigar/game/SteeringBehaviorsSpec.scala
@@ -37,54 +37,24 @@ class SteeringBehaviorSpec extends FlatSpec with Matchers {
 
 
 
-  "WanderingBehavior" should "return a different target from the original one" in {
+  "WanderingBehavior" should "keep the current cell's target on creation" in {
     val cell = new Cell(1, Vector2(0f, 0f))
     cell.target = Vector2(10f, 10f)
     cell.behavior = new WanderingBehavior(cell)
 
     val target = cell.behavior.update(1f)
 
-    target should not be theSameInstanceAs(cell.target)
+    target should be theSameInstanceAs(cell.target)
   }
 
-  it should "successively return different targets (wandering around)" in {
-    val cell = new Cell(1, Vector2(0f, 0f))
+  it should "change target when close enough to the current target" in {
+    val cell = new Cell(1, Vector2(10f, 10f))
     cell.target = Vector2(10f, 10f)
     cell.behavior = new WanderingBehavior(cell)
     
-    val before = cell.behavior.update(1f)
-    val after = cell.behavior.update(1f)
+    val target = cell.behavior.update(1f)
 
-    val distance = before.distanceTo(after)
-
-    // Slight possibility that we get a random angle change of 0 (no change)
-    // We accept this rare event here.
-    distance should be > 0f
-
-    // Specific to wandering behavior: target should be within the circle ahead
-    // of our cell
-    distance should be <= 2 * WanderingBehavior.CircleRadius
-  }
-
-  it should "return a circle center ahead of the cell" in {
-    val cell = new Cell(1, Vector2(5f, 5f))
-    cell.target = Vector2(10f, 5f)
-    val behavior = new WanderingBehavior(cell)
-    cell.behavior = behavior
-
-    behavior.circleCenter should equal(Vector2(5f + WanderingBehavior.CircleDistance, 5f))
-  }
-
-  it should "return a displacement along the circle based on its wander angle" in {
-    val cell = new Cell(1, Vector2(0f, 0f))
-    val behavior = new WanderingBehavior(cell)
-    cell.behavior = behavior
-    behavior.wanderAngle = Pi.toFloat
-
-    val displacement = behavior.displacementOnCircle
-
-    displacement.x should equal(-WanderingBehavior.CircleRadius +- 1e-5f)
-    displacement.y should equal(0f +- 1e-5f)
+    target should not be theSameInstanceAs(cell.target)
   }
 
   it should "change to no behavior on player activity" in {
