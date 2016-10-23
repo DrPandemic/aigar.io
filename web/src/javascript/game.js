b91304b453dd69db4217d0b80f1e95ad8041a33d
Murika <erikad94@hotmail.ca>
2016-10-23 16:26:17 -0400

    remove food from mini-map

--- web/src/javascript/game.js
+++ web/src/javascript/game.js
@@ -1,6 +1,7 @@
 import {
   drawFoodOnMap,
   drawMap,
+  initMiniMap,
   drawMiniMap,
   drawPlayersOnMap,
   initMap,
@@ -10,10 +11,12 @@ const apiURL = "http://localhost:1337/api/1/game/";
 
 export function draw(gameState, canvas) {
   initMap(canvas);
-  drawFoodOnMap(gameState.food, canvas);
   drawPlayersOnMap(gameState.players, canvas);
+  initMiniMap(canvas);
+  drawFoodOnMap(gameState.food, canvas);
   drawMap(canvas);
-  drawMiniMap(canvas);
+  drawMiniMap();
+  
 }
 
 export function fetchState(gameId) {
