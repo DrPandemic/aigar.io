2270bbb77317752e7d0a8ef3c01961a0bedce900
JesseEmond <emond.jesse@gmail.com>
2016-10-24 12:38:22 -0400

    WanderingBehavior change target after some time
    Added as a safety when the velocity of a cell prevents it from reaching
    its target (orbital effect).

--- game/src/test/scala/io/aigar/game/SteeringBehaviorsSpec.scala
+++ game/src/test/scala/io/aigar/game/SteeringBehaviorsSpec.scala
@@ -47,13 +47,25 @@ class SteeringBehaviorSpec extends FlatSpec with Matchers {
     target should be theSameInstanceAs(cell.target)
   }
 
-  it should "change target when close enough to the current target" in {
+  it should "change target when it reaches its target" in {
     val cell = new Cell(1, Vector2(10f, 10f))
     cell.target = Vector2(10f, 10f)
     cell.behavior = new WanderingBehavior(cell)
-    
+
     val target = cell.behavior.update(1f, new Grid(0, 0))
 
+    target should not be theSameInstanceAs(cell.target)
+  }
+
+  it should "change target after a certain delay" in {
+    val cell = new Cell(1, Vector2(0f, 0f))
+    val targetFar = Vector2(100000f, 100000f)
+    cell.target = targetFar
+    cell.behavior = new WanderingBehavior(cell)
+
+    val target = cell.behavior.update(WanderingBehavior.NewTargetDelay + 1e-2f, new Grid(0, 0))
+
+    cell.position.distanceTo(targetFar) should be > 100f // make sure that we really changed because of time (not position)
     target should not be theSameInstanceAs(cell.target)
   }
 
