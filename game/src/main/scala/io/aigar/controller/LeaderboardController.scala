89ccab48fea18b3ef8e84fbe8041dfb2393bd8d3
Jean-Samuel Aubry-Guzzi <DrPandemic@users.noreply.github.com>
2016-10-24 22:35:44 -0400

    Leaderboard api real data (#176)
    * Adds LeaderboardController dependecy to team repo
    
    * Adds tests
    
    * Fetchs the real data

--- game/src/main/scala/io/aigar/controller/LeaderboardController.scala
+++ game/src/main/scala/io/aigar/controller/LeaderboardController.scala
@@ -1,16 +1,16 @@
 package io.aigar.controller
 
 import io.aigar.controller.response._
-import org.json4s.{DefaultFormats, Formats}
+import io.aigar.model.TeamRepository
+
 import org.scalatra.json._
 
-class LeaderboardController extends AigarStack with JacksonJsonSupport {
+class LeaderboardController(teamRepository: TeamRepository) extends AigarStack with JacksonJsonSupport {
   get("/") {
-    LeaderboardResponse(
-      List(
-        LeaderboardEntry(465, "asd", 132),
-        LeaderboardEntry(745, "wow", 2),
-        LeaderboardEntry(7, "such score", 22)
-      ))
+    val teams = teamRepository.getTeams
+      .map(team => {
+             LeaderboardEntry(team.id.get, team.teamName, team.score)
+           })
+    LeaderboardResponse(teams)
   }
 }
--- game/src/test/scala/io/aigar/controller/LeaderboardControllerSpec.scala
+++ game/src/test/scala/io/aigar/controller/LeaderboardControllerSpec.scala
@@ -1,15 +1,33 @@
 import io.aigar.controller._
 import io.aigar.controller.response._
+import io.aigar.model._
 
 import org.json4s._
 import org.json4s.jackson.JsonMethods._
 
 import org.scalatra.test.specs2._
 import org.specs2.matcher._
+import org.specs2.specification.BeforeAfterEach
 
-class LeaderboardControllerSpec extends MutableScalatraSpec {
-  protected implicit val jsonFormats: Formats = DefaultFormats
-  addServlet(classOf[LeaderboardController], "/*")
+class LeaderboardControllerSpec extends MutableScalatraSpec
+    with BeforeAfterEach {
+  implicit val jsonFormats: Formats = DefaultFormats
+  sequential
+
+  val teamRepository = new TeamRepository(None)
+  addServlet(new LeaderboardController(teamRepository), "/*")
+
+  def cleanState = {
+    teamRepository.dropSchema
+    teamRepository.createSchema
+
+    teamRepository.createTeam(Team(Some(1), "EdgQWhJ!v&", "team1", 789))
+    teamRepository.createTeam(Team(Some(2), "EdgQWhJ!v&2", "team2", 50))
+    teamRepository.createTeam(Team(Some(3), "EdgQWhJ!v&3", "team3", 5))
+  }
+
+  def before = cleanState
+  def after = cleanState
 
   "GET / on LeaderboardController" should {
     "return the right data format" in {
@@ -24,6 +42,18 @@ class LeaderboardControllerSpec extends MutableScalatraSpec {
 
         val team_ids = entries.map(_.team_id)
         team_ids.distinct.size must be_==(team_ids.size)
+      }
+    }
+
+    "return an entry for each player" in {
+      get("/") {
+        status must_== 200
+
+        val entries = parse(body).extract[LeaderboardResponse].data
+        val team_ids = entries.map(_.team_id)
+        val db_team_ids = teamRepository.getTeams.map(_.id.get)
+
+        team_ids must be_==(db_team_ids)
       }
     }
   }
