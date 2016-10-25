48f85469fcbb41ac66567d1b6198e8855a7717c0
Mic Lamb <michael.o.lambert@gmail.com>
2016-10-25 17:11:41 -0400

    Adds test for score update
    Adds test for score update

---
+++ game/src/test/scala/io/aigar/model/TeamSpec.scala
@@ -0,0 +1,26 @@
+package io.aigar.model
+
+import org.scalatest._
+
+class TeamSpec extends FlatSpec with Matchers {
+
+  def withInMemDatabase(testCode: (TeamRepository, Team) => Any) {
+    val teamRepository = new TeamRepository(None)
+    val team = teamRepository.createTeam(Team(None, "EdgQWhJ!v&", "team1", 0))
+
+    try{
+      testCode(teamRepository, team)
+    }
+    finally {
+      teamRepository.dropSchema
+      teamRepository.closeConnection
+    }
+  }
+
+  "The TeamDAO" should "update the score accordingly" in withInMemDatabase { (teamRepository, team) =>
+    TeamDAO.updateScore(teamRepository.db, team.id.get, 10)
+    val updatedTeam = TeamDAO.findTeamById(teamRepository.db, team.id.get).get
+
+    updatedTeam.score should equal(team.score + 10)
+  }
+}
