cb66cc0a84dd6df4b88d820ea373cd1500214b17
Murika <erikad94@hotmail.ca>
2016-10-30 13:24:24 -0400

    Revert "rebase"
    This reverts commit 1a54c1bfe7dbe4b7fc393d3f6ada568ca8dced7a.

---
+++ game/src/main/scala/io/aigar/game/ActionQueryWithId.scala
@@ -0,0 +1,9 @@
+package io.aigar.game
+
+import io.aigar.controller.response.Action
+
+/**
+  * Class used to transfer actions from the GameController to the game thread.
+  */
+
+case class ActionQueryWithId(game_id: Int, team_id: Int, actions: List[Action])
