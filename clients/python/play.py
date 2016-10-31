c1497d107109855116b2f7b65f6e0ef1ede77537
JesseEmond <emond.jesse@gmail.com>
2016-10-31 13:24:50 -0400

    Read team secret from player.json

--- clients/python/play.py
+++ clients/python/play.py
@@ -1,4 +1,5 @@
 from time import sleep
+import json
 
 from game.api import API
 from game.models import Game
@@ -9,7 +10,9 @@ UpdatesPerSecond = 3  # how many times we should contact the server per second
 
 def main():
     game_id = Game.RankedGameId
-    api = API("TODO")
+
+    team_secret = fetch_team_secret()
+    api = API(team_secret)
 
     while True:
         game = api.fetch_game_state(game_id)
@@ -18,6 +21,20 @@ def main():
         print(game.tick)
 
         sleep(1 / UpdatesPerSecond)
+
+
+def fetch_team_secret():
+    NullTeamSecret = "REPLACEME"  # value that is by default in the config file
+
+    with open("player.json") as f:
+        data = json.load(f)
+        secret = data["team_secret"]
+
+        if secret == NullTeamSecret:
+            print("WARNING: Did you forget to change your team secret in "
+                  "'player.json'?")
+
+        return secret
 
 
 if __name__ == "__main__":
