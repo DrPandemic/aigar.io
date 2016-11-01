89573d358e0ef2d3bd67a4d3601ed7fe89412f5f
JesseEmond <emond.jesse@gmail.com>
2016-11-01 13:19:53 -0400

    Parse Map object

--- clients/python/game/models_test.py
+++ clients/python/game/models_test.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 
-from .models import Game
+from .models import Game, Map
 
 
 class GameTests(TestCase):
@@ -8,6 +8,10 @@ class GameTests(TestCase):
         obj = {
                 "id": 12,
                 "tick": 123,
+                "map": {
+                    "width": 111,
+                    "height": 222
+                    }
                 # TODO add more here
                 }
 
@@ -15,4 +19,19 @@ class GameTests(TestCase):
 
         self.assertEqual(12, game.id)
         self.assertEqual(123, game.tick)
+        self.assertEqual(111, game.map.width)
+        self.assertEqual(222, game.map.height)
         # TODO add more here
+
+
+class MapTests(TestCase):
+    def test_parse(self):
+        obj = {
+                "width": 123,
+                "height": 321
+                }
+
+        map_ = Map.parse(obj)
+
+        self.assertEqual(123, map_.width)
+        self.assertEqual(321, map_.height)
