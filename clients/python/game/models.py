e53a16c6ffb86fc16b14ffbf9dc0b5e6ace8fdb0
JesseEmond <emond.jesse@gmail.com>
2016-11-01 20:17:40 -0400

    Create Resources class

--- clients/python/game/models.py
+++ clients/python/game/models.py
@@ -90,5 +90,9 @@ class Resources:
                 (len(self.regular), len(self.silver), len(self.gold)))
 
 
+def format_vec2(vec2):
+    return "(%d;%d)" % (vec2.x, vec2.y)
+
+
 def parse_vec2(obj):
     return Vec2(obj["x"], obj["y"])
