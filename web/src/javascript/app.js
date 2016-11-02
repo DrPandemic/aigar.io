98ea7a90b1e85bd313302f7747a22fae1a4fc979
Jean-Samuel Aubry-Guzzi <bipbip500@gmail.com>
2016-11-02 11:02:31 -0400

    Adds better timer and prevent V8 from gcing the web worker

--- web/src/javascript/app.js
+++ web/src/javascript/app.js
@@ -3,21 +3,37 @@ import {drawGame, createGameCanvas} from "./game";
 import {gameRefresh, leaderboardRefresh} from "./constants";
 
 const gameCanvas = createGameCanvas();
+
 let currentState;
+const networkWorker = new Worker("javascript/gameWebWorker.bundle.js");
+networkWorker.onmessage = message => {
+  // This is to prevent Chrome's GC from deleting the worker.
+  // It's happening on Chrome but not on FF.
+  if(!networkWorker) {
+    console.error("Got GCed");
+  }
+  currentState = message.data;
+};
 
-(new Worker("javascript/gameWebWorker.bundle.js")).onmessage = message => currentState = message.data;
-
-async function updateLoop() {
+async function updateGame() {
+  const startTime = (new Date()).getTime();
   if(currentState) {
     drawGame(currentState, gameCanvas);
   }
+
+  const elapsed = (new Date()).getTime() - startTime;
+  setTimeout(updateGame, 1000/gameRefresh - elapsed);
 }
 
 function updateLeaderBoard() {
+  const startTime = (new Date()).getTime();
   if(currentState) {
     drawLeaderboard(currentState);
   }
+
+  const elapsed = (new Date()).getTime() - startTime;
+  setTimeout(updateLeaderBoard, 1000/leaderboardRefresh - elapsed);
 }
 
-setInterval(updateLoop, 1000/gameRefresh);
-setInterval(updateLeaderBoard, 1000/leaderboardRefresh);
+updateGame();
+updateLeaderBoard();
--- web/src/javascript/gameWebWorker.js
+++ web/src/javascript/gameWebWorker.js
@@ -2,7 +2,12 @@ import {fetchState} from "./network";
 import {networkRefresh} from "./constants";
 
 async function updateLoop() {
+  const startTime = (new Date()).getTime();
+
   postMessage(await fetchState(0));
+
+  const elapsed = (new Date()).getTime() - startTime;
+  setTimeout(updateLoop, 1000/networkRefresh - elapsed);
 }
 
-setInterval(updateLoop, 1000/networkRefresh);
+updateLoop();
