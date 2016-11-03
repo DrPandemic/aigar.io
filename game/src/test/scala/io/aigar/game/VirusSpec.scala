18b045a2092ba167a653eebca2c3461363f1b652
Mic Lamb <michael.o.lambert@gmail.com>
2016-11-02 22:56:41 -0400

    Adds tests for detectCollisions

--- game/src/test/scala/io/aigar/game/VirusSpec.scala
+++ game/src/test/scala/io/aigar/game/VirusSpec.scala
@@ -11,4 +11,26 @@ class VirusSpec extends FlatSpec with Matchers {
 
     virus.state should equal(new Position(5, 6))
   }
+
+  it should "not detect a collision when being into a smaller cell" in {
+    val virus = new Virus(new Vector2(5, 5))
+    val player = new Player(1, new Vector2(5, 5))
+    val cell = new Cell(1, player, new Vector2(5, 5))
+
+    cell.mass = 80
+    player.cells = List(cell)
+
+    virus.detectCollisions(List(player)) shouldBe false
+  }
+
+  it should "detect a collision when being into a larger cell" in {
+    val virus = new Virus(new Vector2(5, 5))
+    val player = new Player(1, new Vector2(5, 5))
+    val cell = new Cell(1, player, new Vector2(5, 5))
+
+    cell.mass = 120
+    player.cells = List(cell)
+
+    virus.detectCollisions(List(player)) shouldBe true
+  }
 }
