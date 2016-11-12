d693402da331d969a94faf86c70571e0bf0b155f
Jean-Samuel Aubry-Guzzi <bipbip500@gmail.com>
2016-11-12 12:01:08 -0500

    Puts default duration in a constant

--- game/src/main/scala/io/aigar/game/Game.scala
+++ game/src/main/scala/io/aigar/game/Game.scala
@@ -9,9 +9,10 @@ import io.aigar.controller.response.Action
  */
 object Game {
   final val RankedGameId = 0
+  final val DefaultDuration = 60 * 20
 }
 
-class Game(val id: Int, playerIDs: List[Int], val duration: Int = 60 * 20) {
+class Game(val id: Int, playerIDs: List[Int], val duration: Int = Game.DefaultDuration) {
   val grid = new Grid(playerIDs.length * Grid.WidthPerPlayer, playerIDs.length * Grid.HeightPerPlayer)
   val players = createPlayers
   val viruses = new Viruses(grid)
