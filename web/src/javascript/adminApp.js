7e988964b6fed13067a43c3963cfba52d3299c06
Jean-Samuel Aubry-Guzzi <bipbip500@gmail.com>
2016-11-18 17:45:44 -0500

    Adds duration input

--- web/src/javascript/adminApp.js
+++ web/src/javascript/adminApp.js
@@ -1,6 +1,6 @@
 import {sendAdminRequest} from "./network";
 
-const seedButton = document.getElementById("seedButton");
+const seedButton = document.getElementById("seed-button");
 seedButton.onclick = () => {
   sendAdminRequest("player", "post", {seed: true})
     .then(() => alert("The DB was seeded"))
@@ -11,5 +11,13 @@ const resetButton = document.getElementById("reset-button");
 resetButton.onclick = () => {
   sendAdminRequest("competition", "put", {running: true})
     .then(() => alert("The thread was reset"))
+    .catch(e => alert(e));
+};
+
+const durationButton = document.getElementById("duration-button");
+durationButton.onclick = () => {
+  const duration = parseInt(document.getElementById("duration-input").value);
+  sendAdminRequest("ranked", "put", {duration})
+    .then(() => alert("The duration was set"))
     .catch(e => alert(e));
 };
