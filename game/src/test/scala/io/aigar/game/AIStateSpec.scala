b258726622567603ae609c6afa68685907f498e8
Jean-Samuel Aubry-Guzzi <DrPandemic@users.noreply.github.com>
2016-11-21 19:22:54 -0500

    We need it to be dumber (#352)
    * Changes wandering state and fixes tests
    
    * Adds tests

--- game/src/test/scala/io/aigar/game/AIStateSpec.scala
+++ game/src/test/scala/io/aigar/game/AIStateSpec.scala
@@ -37,48 +37,81 @@ class AIStateSpec extends FlatSpec with Matchers {
     player.aiState shouldBe a [NullState]
   }
 
-  "WanderingState" should "keep the current cell's target on creation" in {
+  "SeekingState" should "keep the current cell's target on creation" in {
     val player = new Player(0, Vector2(0f, 0f))
     val cell = player.cells.head
     cell.target = Vector2(100f, 100f)
-    player.aiState = new WanderingState(player)
+    player.aiState = new SeekingState(player)
 
     val target = player.aiState.update(1f, new Grid(0, 0), cell)
 
     target should be theSameInstanceAs(cell.target)
   }
 
-  it should "change target when it reaches its target" in {
+  it should "change state to SleepingState when it reaches its target" in {
     val player = new Player(0, Vector2(10f, 10f))
     val cell = player.cells.head
     cell.target = Vector2(10f, 10f)
-    player.aiState = new WanderingState(player)
+    player.aiState = new SeekingState(player)
 
-    val target = player.aiState.update(1f, new Grid(0, 0), cell)
+    player.aiState.update(1f, new Grid(0, 0), cell)
 
-    target should not be theSameInstanceAs(cell.target)
+    player.aiState shouldBe a [SleepingState]
   }
 
-  it should "change target after a certain delay" in {
+  it should "change state to SleepingState after a certain delay" in {
     val player = new Player(0, Vector2(0f, 0f))
     val cell = player.cells.head
     val targetFar = Vector2(100000f, 100000f)
     cell.target = targetFar
-    player.aiState = new WanderingState(player)
+    player.aiState = new SeekingState(player)
 
     val target = player.aiState.update(WanderingState.NewTargetDelay + 1e-2f, new Grid(0, 0), cell)
 
     cell.position.distanceTo(targetFar) should be > 100f // make sure that we really changed because of time (not position)
-    target should not be theSameInstanceAs(cell.target)
+    player.aiState shouldBe a [SleepingState]
   }
 
   it should "change to null state on player activity" in {
     val player = new Player(0, Vector2(5f, 5f))
     val cell = player.cells.head
-    player.aiState = new WanderingState(player)
+    player.aiState = new SeekingState(player)
 
     player.aiState.onPlayerActivity
 
     player.aiState shouldBe a [NullState]
+  }
+
+  "SleepingBehavior" should "returns the cells's position as the new target" in {
+    val player = new Player(0, Vector2(0f, 0f))
+    val cell = player.cells.head
+    cell.target = Vector2(100f, 100f)
+    player.aiState = new SleepingState(player)
+
+    val target = player.aiState.update(1f, new Grid(0, 0), cell)
+
+    target should be theSameInstanceAs(cell.position)
+  }
+
+  it should "change cell's state to SeekingState after a certain delay" in {
+    val player = new Player(0, Vector2(0f, 0f))
+    val cell = player.cells.head
+    cell.target = Vector2(100f, 100f)
+    player.aiState = new SleepingState(player)
+
+    player.aiState.update(WanderingState.SleepingDelay + 1e-2f, new Grid(0, 0), cell)
+
+    player.aiState shouldBe a [SeekingState]
+  }
+
+  it should "change the cell's target after a certain delay" in {
+    val player = new Player(0, Vector2(0f, 0f))
+    val cell = player.cells.head
+    cell.target = Vector2(100f, 100f)
+    player.aiState = new SleepingState(player)
+
+    val target = player.aiState.update(WanderingState.SleepingDelay + 1e-2f, new Grid(0, 0), cell)
+
+    target shouldNot be theSameInstanceAs(cell.target)
   }
 }
