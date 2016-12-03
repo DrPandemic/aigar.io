ab30b8a3d85f632fdcc525b28bfbc5c39bd4cbb4
Jean-Samuel Aubry-Guzzi <bipbip500@gmail.com>
2016-12-03 17:09:32 -0500

    Adds leaderboard transition

--- web/src/javascript/gameLeaderboard.js
+++ web/src/javascript/gameLeaderboard.js
@@ -3,29 +3,70 @@ import {getPlayerColor, setFocusScreen, cellFocused, findNextCell} from "./game"
 import sort from "immutable-sort";
 
 export function drawLeaderboard(state) {
-  const leaderboard = document.getElementById("leaderboard");
+  const tbody = document.getElementById("leaderboard-body");
+
+  const sortedPlayers = sort(state.players, (a, b) => b.total_mass - a.total_mass);
+  // Initial case
+  if(tbody.rows.length === 0) {
+    createTable(sortedPlayers, null, true);
+    return;
+  }
+
+  const oldBounding = [... tbody.rows].reduce((result, row) => {
+    result[row.id] = row.getBoundingClientRect();
+    return result;
+  }, {});
+
+  createTable(sortedPlayers, oldBounding, false);
+}
+
+function createTable(players, oldBounding, firstTime) {
   const new_tbody = document.createElement("tbody");
   new_tbody.setAttribute("id", "leaderboard-body");
-  const players = sort(state.players, (a, b) => b.total_mass - a.total_mass);
 
-  for(const player of players) {
+  for(const playerIndex in players) {
+    const player = players[playerIndex];
     const color = getPlayerColor(players, player);
+
     const row = new_tbody.insertRow();
+    row.id = `player-${player.id}`;
     row.onclick = function() {
       focusOnPlayer(player);
     };
+
     if(!player.isActive) {
       row.className = "inactive-player";
     }
+
     row.insertCell(0).innerHTML = `<div class="color-box" style="background-color: ${color};"></div>`;
     row.insertCell(1).innerHTML = he.encode(player.id.toString());
     row.insertCell(2).innerHTML = he.encode(player.name);
     row.insertCell(3).innerHTML = he.encode(player.total_mass.toString());
+
+    if(!firstTime) {
+      addTransition(oldBounding, row, playerIndex);
+    }
   }
 
   const old = document.getElementById("leaderboard-body");
+  const leaderboard = document.getElementById("leaderboard");
   leaderboard.replaceChild(new_tbody, old);
   cleanEventListeners(old);
+}
+
+function addTransition(oldBounding, row, playerIndex) {
+  const header = document.getElementById("leaderboard-header").getBoundingClientRect();
+
+  const deltaY = oldBounding[row.id].top - (playerIndex * (header.bottom - header.top) + header.bottom);
+
+  requestAnimationFrame(() => {
+    row.style.transform = `translate(0, ${deltaY}px)`;
+    row.style.transition = "transform 0s";
+    requestAnimationFrame(() => {
+      row.style.transform = "";
+      row.style.transition = "transform 0.5s";
+    });
+  });
 }
 
 function cleanEventListeners(old) {
