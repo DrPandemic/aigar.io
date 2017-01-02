90d03f6a851f461dfa29d1751f889f0d177fcb9c
DrPandemic <bipbip500@gmail.com>
2017-01-02 11:12:37 -0500

    Adds map

--- clients/javascript/game/map.js
+++ clients/javascript/game/map.js
@@ -1,1 +1,10 @@
+module.exports = class Map {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
 
+  static parse(payload) {
+    return new Map(payload.width, payload.height);
+  }
+};
---
+++ clients/javascript/test/game/map_spec.js
@@ -0,0 +1,16 @@
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+chai.use(chaiAsPromised);
+const expect = chai.expect;
+
+const responseExample = require('./state_response_example.json');
+const Map = require('../../game/map.js');
+
+describe('Map', function() {
+  it('parses correctly', function() {
+    const resources = Map.parse(responseExample.data.map);
+
+    expect(resources).to.have.property('width').to.be.equal(1500);
+    expect(resources).to.have.property('height').to.be.equal(1500);
+  });
+});
