85cadbd118299138a1a0da950a9eb8e6651e7806
dysleixa <emond.jesse@gmail.com>
2018-05-28 20:45:06 -0400

    Split push forces (#376)
    * Add randomUnitVector function in Vector2Utils
    
    * Add perpendicular Vector2 addon
    
    * Push cells on split
    
    * Add split push force test
    
    * Add applySplitPushBack tests
    
    * Remove unnecessary Vector2Utils import

--- game/src/main/scala/io/aigar/game/Vector2Utils.scala
+++ game/src/main/scala/io/aigar/game/Vector2Utils.scala
@@ -1,9 +1,16 @@
 package io.aigar.game
 
+import scala.util.Random
+import scala.math.{Pi, cos, sin}
 import com.github.jpbetz.subspace._
 import io.aigar.game.serializable.Position
 
 object Vector2Utils {
+  def randomUnitVector: Vector2 = {
+    val angle = Random.nextFloat * 2f * Pi.toFloat
+    Vector2(cos(angle).toFloat, sin(angle).toFloat)
+  }
+
   implicit class Vector2Addons(val vector: Vector2) {
     def state = {
       serializable.Position(vector.x, vector.y)
@@ -15,6 +22,13 @@ object Vector2Utils {
 
     def safeNormalize: Vector2 = {
       if (vector.magnitude > 0f) vector.normalize else vector
+    }
+
+    /**
+     * Perpendicular (clockwise) version of the vector.
+     */
+    def perpendicular: Vector2 = {
+      return Vector2(-vector.y, vector.x)
     }
   }
 }
