a189437de09f491988ad9d292e64755618e5c1ec
Emile Fugulin <emilefugulin@hotmail.com>
2018-06-02 17:14:26 -0400

    Change logger in config reader and remove setters

--- clients/java/src/main/java/io/aigar/config/Config.java
+++ clients/java/src/main/java/io/aigar/config/Config.java
@@ -9,23 +9,11 @@ public class Config {
         return playerId;
     }
 
-    public void setPlayerId(int playerId) {
-        this.playerId = playerId;
-    }
-
     public String getPlayerSecret() {
         return playerSecret;
     }
 
-    public void setPlayerSecret(String playerSecret) {
-        this.playerSecret = playerSecret;
-    }
-
     public String getApiUrl() {
         return apiUrl;
-    }
-
-    public void setApiUrl(String apiUrl) {
-        this.apiUrl = apiUrl;
     }
 }
--- clients/java/src/main/java/io/aigar/config/ConfigReader.java
+++ clients/java/src/main/java/io/aigar/config/ConfigReader.java
@@ -1,16 +1,17 @@
 package io.aigar.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+
 
 public class ConfigReader {
     private static final String ConfigFileName = "/player.json";
 
-    private static final Logger logger = Logger.getLogger(ConfigReader.class.getName());
+    private static final Logger logger = LoggerFactory.getLogger(ConfigReader.class);
 
     public Config readConfig() throws IOException {
         try {
@@ -20,7 +21,7 @@ public class ConfigReader {
 
             return mapper.readValue(data, Config.class);
         } catch (Exception e) {
-            logger.log(Level.SEVERE, "Could not load the config from '{}'", ConfigFileName);
+            logger.error("Could not load the config from '{}'", ConfigFileName);
             throw e;
         }
     }
